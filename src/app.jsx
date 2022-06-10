e32755f2b75f1ef7d8899b848de38e39a4e054ee
JiHwan Lee <kdgosu53@gmail.com>
2022-06-10 18:00:51 +0900

    router모듈 설치

--- src/app.jsx
+++ src/app.jsx
@@ -2,13 +2,17 @@ import logo from "./logo.svg";
 import logo2 from "./logo192.png";
 import styles from "./app.module.css";
 import Login from "./components/login/login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <div className={styles.container}>
-        <img src={logo2} alt="" />
-        <Login />
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   );
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./login.module.css";
 import { signInWithGoogle, signInWithGithub } from "../../service/fireBase.js";
+import logo2 from "../../logo192.png";
 
 const Login = () => {
   const onSnsBtnClick = (e) => {
@@ -16,18 +17,21 @@ const Login = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <h2 className={styles.title}>Business Card Maker</h2>
-      <div className={styles.content}>
-        <h2> Login </h2>
-        <button className={styles.btn} onClick={signInWithGoogle} name="google">
-          Google
-        </button>
-        <button className={styles.btn} onClick={signInWithGithub}>
-          Github
-        </button>
+    <div className={styles.wrapper}>
+      <img src={logo2} alt="" />
+      <div className={styles.container}>
+        <h2 className={styles.title}>Business Card Maker</h2>
+        <div className={styles.content}>
+          <h2> Login </h2>
+          <button className={styles.btn} onClick={signInWithGoogle}>
+            Google
+          </button>
+          <button className={styles.btn} onClick={signInWithGithub}>
+            Github
+          </button>
+        </div>
+        <h2 className={styles.footer}> Code your dream</h2>
       </div>
-      <h2 className={styles.footer}> Code your dream</h2>
     </div>
   );
 };
