a313bb8a0c11166f5c161256abe33cc52b28789f
JiHwan Lee <kdgosu53@gmail.com>
2022-06-10 13:13:18 +0900

    firebase google 로그인 구현

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import styles from "./login.module.css";
+import { signInWithGoogle } from "../../service/fireBase.js";
 
 const Login = () => {
-  const onSnsBtnClick = (v) => {
+  const onSnsBtnClick = (e) => {
     // alert(`v: ${v}`);
     console.log(`onSnsBtncLICK`);
+    console.log(e);
+    const name = e.target.name;
+
+    if (name === "google") {
+      //   provider = new firebase() ;
+      //   signInWithGoogle;
+    }
   };
 
   return (
@@ -12,12 +20,11 @@ const Login = () => {
       <h2 className={styles.title}>Business Card Maker</h2>
       <div className={styles.content}>
         <h2> Login </h2>
-        <button className={styles.btn} onClick={onSnsBtnClick("google")}>
+        <button className={styles.btn} onClick={signInWithGoogle} name="google">
           Google
         </button>
-        <button className={styles.btn} onClick={onSnsBtnClick("github")}>
-          {" "}
-          Github{" "}
+        <button className={styles.btn} onClick={onSnsBtnClick}>
+          Github
         </button>
       </div>
       <h2 className={styles.footer}> Code your dream</h2>
---
+++ src/service/fireBase.js
@@ -0,0 +1,51 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKED,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const provider = new GoogleAuthProvider();
+const auth = getAuth();
+
+export const signInWithGoogle = () => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      console.log(token);
+      console.log(user);
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+};
+
+export const authService = getAuth();
