8fd3635dcbf3bf038714cab902489b80f68a3f72
JiHwanLee <kdgosu53@gmail.com>
2022-06-18 17:03:20 +0900

    cardItem 컴포넌트 form태그로 구현

--- src/components/cardmaker/carditem.jsx
+++ src/components/cardmaker/carditem.jsx
@@ -1,35 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./carditem.module.css";
 
 const Carditem = () => {
+  console.log(`CardItem()`);
+  const [name, setName] = useState("");
+  const [company, setCompany] = useState("");
+  const [color, setColor] = useState("");
+  const [title, setTitle] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [file, setFile] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("handleSubmit");
+    console.log(e);
+    console.log(`name : ${name}`);
+    console.log(`company : ${company}`);
+    console.log(`color : ${color}`);
+    console.log(`title : ${title}`);
+    console.log(`email : ${email}`);
+    console.log(`message : ${message}`);
+    console.log(`file : ${file}`);
+    console.log(file);
+
+    setName("");
+  };
+
+  const handleSetFile = (e) => {
+    console.log(e.target.files[0]);
+    setFile(e.target.files[0]);
+  };
+
   return (
     <div className={styles.container}>
-      <div className={styles.content1}>
-        <input type="text" placeholder="Name" className={styles.inputName} />
-        <input
-          type="text"
-          placeholder="Company"
-          className={styles.inputCompany}
-        />
+      <form onSubmit={handleSubmit}>
+        <div className={styles.content1}>
+          <input
+            type="text"
+            placeholder="Name"
+            className={styles.inputName}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Company"
+            className={styles.inputCompany}
+            onChange={(e) => setCompany(e.target.value)}
+          />
 
-        <select name="" id="" className={styles.selectColor}>
-          <option value=""> Light</option>
-          <option value=""> Dark</option>
-          <option value=""> Colorful</option>
-        </select>
-      </div>
-      <div className={styles.content2}>
-        <input type="text" placeholder="Title" className={styles.inputTitle} />
-        <input type="text" placeholder="Email" className={styles.inputEmail} />
-      </div>
-      <div className={styles.content3}>
-        <textarea placeholder="Message" className={styles.inputTitle} />
-      </div>
+          <select
+            name="selectColor"
+            id="selectColor"
+            className={styles.selectColor}
+            onChange={(e) => setColor(e.target.value)}
+          >
+            <option value="Light"> Light</option>
+            <option value="Dark"> Dark</option>
+            <option value="Colorful"> Colorful</option>
+          </select>
+        </div>
+        <div className={styles.content2}>
+          <input
+            type="text"
+            placeholder="Title"
+            className={styles.inputTitle}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            className={styles.inputEmail}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className={styles.content3}>
+          <textarea
+            placeholder="Message"
+            className={styles.inputTitle}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </div>
 
-      <div className={styles.content4}>
-        <button className={styles.btnFile}> No file </button>
-        <button className={styles.btnAdd}> Add </button>
-      </div>
+        <div className={styles.content4}>
+          <label className={styles.btnFile} for="file">
+            No file
+          </label>
+          <input
+            type="file"
+            id="file"
+            style={{ display: "none" }}
+            onChange={handleSetFile}
+          />
+          <input type="submit" className={styles.btnAdd} value="Add" />
+
+          {/* <button className={styles.btnAdd}> Add </button> */}
+        </div>
+      </form>
     </div>
   );
 };
--- src/components/cardmaker/cardmaker.jsx
+++ src/components/cardmaker/cardmaker.jsx
@@ -5,6 +5,7 @@ const Cardmaker = () => {
   return (
     <>
       <span> Card Maker </span>
+      {/* 반복문으로 파이어베이스에 등록된 카드정보를 불러옴.  */}
       <Carditem />
     </>
   );
