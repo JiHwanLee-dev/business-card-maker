03be75a926eb71c5047b87433ba04e86bfe4c4aa
JiHwan Lee <kdgosu53@gmail.com>
2022-06-29 16:14:15 +0900

    cardpreviewitem 데이터 받아와서 렌더링 작업 완료

--- src/components/cardpreview/cardpreview.jsx
+++ src/components/cardpreview/cardpreview.jsx
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Cardpreviewitem from "../cardpreviewitem/cardpreviewitem";
 import styles from "./cardpreview.module.css";
+import { UserContext } from "../../providers/userprovider";
+import { dbRead } from "../../service/fireBase";
 
 const Cardpreview = () => {
+  const user = useContext(UserContext);
+  const [cards, setCards] = useState([]);
+
+  console.log("Cardpreview()");
+  useEffect(() => {
+    if (user) {
+      dbRead(user.uid, (res) => {
+        if (res !== null) {
+          console.log(res);
+          let strArr = Object.keys(res); // object to array
+          let data = [];
+          strArr.map((value) => {
+            let key = { key: value };
+            data.push(Object.assign(key, res[value]));
+          });
+          // data.push(res);
+
+          console.log(data);
+
+          setCards(data);
+        } else {
+          setCards([]);
+        }
+      });
+    }
+  }, []);
+
   return (
     <div className={styles.cardpreview}>
       <span> Card Preview </span>
-      <Cardpreviewitem />
+      <ul className={styles.cards}>
+        {cards.map((card, index) => (
+          <li className={styles.card_li} key={index}>
+            <Cardpreviewitem key={card.key} card={card} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
--- src/components/cardpreviewitem/cardpreviewitem.jsx
+++ src/components/cardpreviewitem/cardpreviewitem.jsx
@@ -1,31 +1,68 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styles from "./cardpreviewitem.module.css";
+import { UserContext } from "../../providers/userprovider";
+import { dbRead } from "../../service/fireBase";
 
-const Cardpreviewitem = () => {
+const Cardpreviewitem = (props) => {
+  console.log("CardPreviewItem()");
+  console.log(props);
+
+  //   const container = useRef();
+
+  const [color, setColor] = useState("");
+
+  const aa = "dark";
+  const element = <h1>Hello, world!</h1>;
+  const light = "1px solid red";
+  const dark = "1px solid black";
+
+  let backColor = "1px solid blue";
+
+  if (props.card.color === "Light") {
+    backColor = styles.light;
+  } else if (props.card.color === "Dark") {
+    backColor = styles.dark;
+  } else if (props.card.color === "Colorful") {
+    backColor = styles.colorful;
+  }
+
+  useEffect(() => {
+    // console.log("useEffecrt()");
+    // if (props.card.color === "Light") {
+    //   console.log("aaaaaaaaaaaaaa");
+    //   backColor = "1px solid red";
+    // } else if (props.card.color === "Dark") {
+    //   backColor = "1px solid black";
+    // }
+  }, []);
+
   return (
-    <div className={styles.container}>
-      <div className={styles.img}>
-        <img src="../../logo192.png" alt="" />
+    <>
+      <div
+        className={`${styles.container} ${backColor}`}
+        style={{ border: backColor }}
+      >
+        <div className={styles.img}>
+          <img src="../../logo192.png" alt="" />
+        </div>
+        <div className={styles.content}>
+          <div>
+            <span className={styles.name}> {props.card.name} </span>
+          </div>
+          <div>
+            <span> {props.card.company} </span>
+          </div>
+          <div className={styles.line}></div>
+          <div>
+            <span> {props.card.title} </span>
+          </div>
+          <div>
+            <span> {props.card.email} </span>
+          </div>
+          <div className={styles.message}>"{props.card.message}"</div>
+        </div>
       </div>
-      <div className={styles.content}>
-        <div>
-          <span className={styles.name}> Name </span>
-        </div>
-        <div>
-          <span> Company </span>
-        </div>
-        <div className={styles.line}></div>
-        <div>
-          <span> Title </span>
-        </div>
-        <div>
-          <span> Eamil </span>
-        </div>
-        <div>
-          <span> Message </span>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
